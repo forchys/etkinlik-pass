16bb6b0d315c69f30e18c05a5deaf84f620fe5e1
forchys <aerendogan@icloud.com>
2026-04-25 15:35:40 +0300

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 import { useState, useEffect, createContext, useContext } from 'react';
-import { supabase } from '../../lib/supabase';
+import { supabase } from '@/lib/supabase';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { 
